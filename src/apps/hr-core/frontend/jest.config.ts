f7e4927bf844dd7fa2973255e325783803f30d3c
michifueby <michi.fueby@gmail.com>
2025-11-24 16:27:31 +0100

    feat: Add backend for each service

--- src/apps/hr-core/frontend/jest.config.ts
+++ src/apps/hr-core/frontend/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
   displayName: 'frontend',
-  preset: '../../../jest.preset.js',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../../coverage/apps/hr-core/frontend',
+  coverageDirectory: '../../coverage/hr-core/frontend',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
--- src/apps/hr-metrics/frontend/jest.config.ts
+++ src/apps/hr-metrics/frontend/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
   displayName: 'frontend',
-  preset: '../../../jest.preset.js',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../../coverage/apps/hr-metrics/frontend',
+  coverageDirectory: '../../coverage/hr-metrics/frontend',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
