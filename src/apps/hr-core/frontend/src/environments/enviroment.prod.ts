535ecc87e8359d69c9ad672246b1110fcfeb5075
michifueby <michi.fueby@gmail.com>
2025-11-27 19:29:55 +0100

    Fix assets and update prod env

--- src/apps/hr-core/frontend/src/environments/enviroment.prod.ts
+++ src/apps/hr-core/frontend/src/environments/enviroment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://http://hrm-core-backend-env.eba-mpjkpkpv.eu-central-1.elasticbeanstalk.com',
+  apiUrl: 'http://http://hrm-core-backend-env.eba-mpjkpkpv.eu-central-1.elasticbeanstalk.com/api/time',
   metricsUrl: 'http://hrm-metrics-backend-env.eba-mpjkpkpv.eu-central-1.elasticbeanstalk.com',
   activitiesUrl: 'http://hrm-activities-backend-env.eba-mpjkpkpv.eu-central-1.elasticbeanstalk.com'
 };
